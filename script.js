c08af7cb5dd43f0316fa704d252750bbe23d9551
dudleydehenau <skydrmddh@gmail.com>
2024-01-08 20:57:30 +0100

    Sketch WebSite

---
+++ script.js
@@ -0,0 +1,16 @@
+function toggleMenu() {
+    const sidebar = document.querySelector('.sidebar');
+    const menuIcon = document.querySelector('.menu-icon');
+
+    const isSidebarVisible = sidebar.classList.contains('show-sidebar');
+
+   
+    sidebar.classList.toggle('show-sidebar', !isSidebarVisible);
+
+    
+    if (!isSidebarVisible) {
+        menuIcon.style.transform = 'translateX(250px)';
+    } else {
+        menuIcon.style.transform = 'translateX(0)';
+    }
+}
